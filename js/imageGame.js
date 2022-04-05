dd44989c82ab9e4206d83bd206047676293e73b9
Brendan Degryse <19598@my4County.net>
2022-04-05 12:36:29 -0400

    Update imageGame.js

--- js/imageGame.js
+++ js/imageGame.js
@@ -1,20 +1,19 @@
 "use strict";
-window.onload = function() {
-    let images = document.getElementsByTagName("img");
-    for (let i = 0; i < images.length; i++) {
-        images[i].onclick = showAnswer;
-    }
-};
+//When the window loads, go through each image tag and set its onclick to the showAnswer function
+window.onload = () => Array.from(document.getElementsByTagName("img")).forEach(i => i.onclick = showAnswer);
 
+//When the user clicks the image, run this function that changes to the unblurred image
 function showAnswer(eventObj) {
     let image = eventObj.target;
     let name = image.id;
     name = "pics/" + name + ".jpg";
     image.src = name;
-
+	
+	//Wait 20 seconds and reblur
     setTimeout(reblur, 2000, image);
 }
 
+//Sets the image to the blurred version
 function reblur(image) {
     let name = image.id;
     name = "pics/" + name + "blur.jpg";
